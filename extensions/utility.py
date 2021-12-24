090dacac4d024e9c21b8c9e7f436d8e2a17e9dc8
Burrito <37681398+burritosoftware@users.noreply.github.com>
2021-12-23 22:35:31 -0800

    Ephemeral messages

--- extensions/utility.py
+++ extensions/utility.py
@@ -4,7 +4,7 @@ import os
 ping_plugin = lightbulb.Plugin("Ping")
 
 @ping_plugin.command
-@lightbulb.command("ping", description="Get the bot's ping.")
+@lightbulb.command("ping", description="Get the bot's ping.", ephemeral=True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def ping(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"Pong! Latency: {ctx.bot.heartbeat_latency*1000:.2f}ms")
@@ -12,7 +12,7 @@ async def ping(ctx: lightbulb.Context) -> None:
 update_plugin = lightbulb.Plugin("Update")
 
 @update_plugin.command
-@lightbulb.command("update", description="Pulls the latest source from GitHub and updates the bot.")
+@lightbulb.command("update", description="Pulls the latest source from GitHub and updates the bot.", ephemeral=True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def update(ctx: lightbulb.Context) -> None:
     if str(ctx.author.id) != "261236127581601793":
