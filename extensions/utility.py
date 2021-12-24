f63edcdc47b811c26dbfa2913e7a2ece97e96128
Burrito <37681398+burritosoftware@users.noreply.github.com>
2021-12-23 22:03:48 -0800

    Test feex

--- extensions/utility.py
+++ extensions/utility.py
@@ -12,7 +12,7 @@ async def ping(ctx: lightbulb.Context) -> None:
 update_plugin = lightbulb.Plugin("Update")
 
 @update_plugin.command
-@lightbulb.command("update", description="Pulls the latest source from GitHub and updates the bot.")
+@lightbulb.command("update", description="Pulls the latest release from GitHub and updates the bot.")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def update(ctx: lightbulb.Context) -> None:
     if str(ctx.author.id) != "261236127581601793":
@@ -23,9 +23,8 @@ async def update(ctx: lightbulb.Context) -> None:
             await ctx.respond("Bot already up to date.")
         else:
             await ctx.respond("Update complete, sending PM2 the restart signal on Linux, or restart manually now.")
-            if __name__ == "__main__":
-                if os.name != "nt":
-                    os.system("pm2 restart bot")
+            if os.name != "nt":
+                os.system("pm2 restart bot")
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(ping_plugin)
